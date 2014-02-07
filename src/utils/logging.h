fecf65dab6962acbfb07007793e65ee865f44cac
robal <robal@tls.pl>
2014-02-07 20:51:47 +0100

    TRACE_PARAMS macro

--- src/utils/logging.h
+++ src/utils/logging.h
@@ -5,9 +5,16 @@
 #define TRACE(message) std::cerr << message
 #define TRACELN(message) std::cerr << message << std::endl
 
+#define _TRACE_PARAM(p) #p << " = " << p
+#define _LEFT_SHIFT() << ", " <<
+#define TRACE_PARAMS(...) \
+    std::cerr << FOREACH(_TRACE_PARAM, _LEFT_SHIFT, __VA_ARGS__) << std::endl;
+
 #else
 
 #define TRACE(message)
 #define TRACELN(message)
+#define TRACE_PARAMS(...)
 
 #endif
+
